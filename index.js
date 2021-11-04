690a3c0395a3cd4b4e7285381f39b9449725342e
+branden <+branden_ott@outlook.com+>
2021-11-04 15:48:53 -0400

    commit tree 1e21120c23994ea4904f05f851f036e5d7c4960f

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution in this file!
+const  returnFirstTwoDrivers = function (drivers) {
+    return drivers.slice(0,2)
+
+};
+const returnLastTwoDrivers = function (drivers) {
+    return drivers.slice(-2)
+};
+
+const selectingDrivers =  [returnFirstTwoDrivers,returnLastTwoDrivers]
+ 
+const createFareMultiplier = function (a) {
+    return function (b) {
+        return a * b
+    } 
+
+};
